df59533a8b1f58e49b850b839fe5294bae9a15b1
yuxing <458157879@qq.com>
2022-07-10 10:04:59 +0800

    hot-fix first commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello github-idea");
+        System.out.println("hello github4");
     }
 }
